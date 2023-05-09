d97ed8089bd3b33edaa2c3020cf3ed182120b0f5
Ginachiko1998 <soniagina669@gmail.com>
2023-05-10 01:28:54 +0700

    make change on pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
-import Home from "./page/Home";
+import Home from "./components/Home";
 import { AuthContextProvider } from "./context/AuthContext";
-
 
 function App() {
   return (
     <AuthContextProvider>
-        <div className="App">
-          <Home/>
-        </div>
+      <div className="App">
+        <Home />
+      </div>
     </AuthContextProvider>
   );
 }
--- src/newsSaga.js
+++ src/newsSaga.js
@@ -3,12 +3,10 @@ import { getNewsSuccess } from "./newsState";
 import axios from "axios";
 
 const apiKey = "73f05d004bb047ec897b15d66ebcbddf";
-let limit = 5;
-const apiUrl = `https://mocki.io/v1/e9063fe4-37df-418e-8741-16b3f49c25d0?_page=1&_limit=${limit}`;
 
 function* workgetNewsFetch(action) {
   try {
-    const { limit, currentPage } = action.payload;
+    const { limit = 10, currentPage = 5 } = action.payload || {};
     const apiUrl = `https://mocki.io/v1/e9063fe4-37df-418e-8741-16b3f49c25d0?_page=${currentPage}&_limit=${limit}`;
     const response = yield call(() => axios.get(`${apiUrl}&apiKey=${apiKey}`));
     const formattedNewsShortened = response.data.articles.slice(0, 5);
