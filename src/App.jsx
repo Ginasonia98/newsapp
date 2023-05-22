eae023587d4c8b137777a94a5957378881bcd066
Ginachiko1998 <soniagina669@gmail.com>
2023-05-22 20:35:01 +0700

    delete home in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Home from "./components/Home";
+import Home from "./page/Home";
 import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
--- src/newsSaga.js
+++ src/newsSaga.js
@@ -8,13 +8,16 @@ function* workgetNewsFetch(payload) {
     const apiUrl = `https://mocki.io/v1/0e45cb1c-e75a-4dca-8a28-5815671a61ab?country=id`;
     const response = yield call(() => axios.get(apiUrl));
     const totalPage = Math.ceil(response.data.articles.length / perPage);
+    /**Hitung total halaman yang diperlukan berdasarkan jumlah artikel yang diterima dari respons, dibagi dengan perPage. */
     const news = [];
     for (let index = 0; index < totalPage; index++) {
       const content = response.data.articles.slice(
         index * perPage,
         (index + 1) * perPage
       );
+      /**Lakukan iterasi sebanyak totalPage, pada setiap iterasi lakukan slicing pada array articles dengan memperhitungkan perPage dan simpan ke dalam variabel content. */
       news.push(content);
+      /**Masukkan content ke dalam array news */
     }
     yield put(getNewsSuccess(news));
   } catch (error) {
